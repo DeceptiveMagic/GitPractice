5c9d26b99e0da003a35228ad118ff865f6c64835
Andrew Hua <andrewhua08@gmail.com>
2022-09-30 18:39:16 -0400

    Cool commit

--- GitPractice.java
+++ GitPractice.java
@@ -1,7 +1,15 @@
-package git;
+package CoolPackage;
 
 public class GitPractice {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		AHHHHHHHHHH
+		AHHHHHHHHHHHHH
+		AHHHHHHHHHHHHHHHHHHHHh
+		AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+		njvebmfdwowoj nw
+		Hi my name is Barry allen
+		and im the fastest man alive
+		im apart of the speed force
 	}
 }
