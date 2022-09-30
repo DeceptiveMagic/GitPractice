8613e33bfb0287331ffbf3ff7b50c91eb1d98d61
Gabe <deceptivemagic9@gmail.com>
2022-09-30 18:41:01 -0400

    Get committed idiot *fun text

--- GitPractice.java
+++ GitPractice.java
@@ -4,4 +4,9 @@ public class GitPractice {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 	}
+	
+	sgerphjiosgrhiopgahuiop
+	
+	hiopuiovwa eouiyw
+	//This is a comment
 }
